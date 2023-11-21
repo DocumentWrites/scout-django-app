f5ea17e362046ef8a755dd8200caeb6f97456401
charles-the-developer <charlesmuzonzini@gmail.com>
2023-11-21 23:35:43 +1100

    init commit

---
+++ blog/forms.py
@@ -0,0 +1,16 @@
+# blog/forms.py
+
+from django import forms
+
+class CommentForm(forms.Form):
+    author = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={ "class": "form-control", "placeholder": "Charlie" }
+        ),
+    )
+    body = forms.CharField(
+        widget=forms.Textarea(
+            attrs={ "class": "form-control", "placeholder": "Let us know what you think!!" }
+        )
+    )
